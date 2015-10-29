21ca66ee74cfa8291ba8f089a83a657ca206acd7
dgslipp <davidslipp@Davids-MacBook-Pro.local>
2015-10-29 06:31:51 -0400

    check if your require bananas, and print the request

--- exercise6.rb
+++ exercise6.rb
@@ -4,9 +4,13 @@ $grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 def grocery(g)
 	$grocery_list.push(g)
 end	
-
 grocery(g)
-	$grocery_list.each do |n|
-	puts "* #{n}"
+$grocery_list.each do |x|
+$b = (x == "bananas"? "y":"n")
+# x == "bananas"
+	puts "* #{x}"
 end
+puts $b
+# puts (b==true ? "You don't need to pick up bananas today":"You need to pick up bananas today")
+puts ($b=="y"? "You don't need to pick up bananas today":"You need to pick up bananas today")
 puts $grocery_list.count
